38c8c822d72a753e13b319ce918b21964e32935f
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-03-09 10:50:27 +0000

    Update v2.py

--- v2_agumentaiton/v2.py
+++ v2_agumentaiton/v2.py
@@ -12,6 +12,7 @@ transform_geo1 = v2.Compose([
 transform_geo2 = v2.Compose([
     v2.ElasticTransform(alpha=300.0),
     v2.RandomResizedCrop(size=(224, 224), antialias=True)
+    
 ])
 
 transforms_geo =  v2.RandomChoice([
@@ -19,6 +20,19 @@ transforms_geo =  v2.RandomChoice([
     transform_geo2,
     v2.Compose([])  # No transform
 ], p=[0.3, 0.2, 0.5])
+
+transform_inten1 = v2.RandomChoice([
+    v2.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5.)),
+    v2.RandomAdjustSharpness(sharpness_factor=4),
+    v2.RandomEqualize(),
+],p=[0.3, 0.4, 0.3])
+
+transforms_inten =  v2.RandomChoice([
+    transform_inten1,
+    v2.Compose([])  # No transform
+], p=[0.5, 0.5])
+
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,18 +42,18 @@ path_st_img = r"C:\My_Data\KeenAI\rust_new\DEC_15\val\steel\5HCIwWB8zye2BJ6hmj9R
 path_label = r"C:\My_Data\KeenAI\rust_new\DEC_15\val\gts\5HCIwWB8zye2BJ6hmj9RA==_1701.png"
 
 
-img = cv2.imread(path_st_img)
-st_img = cv2.imread(path_img)
+img = cv2.imread(path_img)
+st_img = cv2.imread(path_st_img)
 label = cv2.imread(path_label)*255
 
-# plt.figure()
-# plt.imshow(img)
+plt.figure()
+plt.imshow(img)
 
-# plt.figure()
-# plt.imshow(st_img)
+plt.figure()
+plt.imshow(st_img)
 
-# plt.figure()
-# plt.imshow(label)
+plt.figure()
+plt.imshow(label)
 
 
 img = np.moveaxis(img,-1,0)
@@ -54,10 +68,14 @@ all_three = torch.tensor(all_three)
 #transformed_images = T.RandomRotation(180)(all_three)
 transformed_images = transforms_geo(all_three)
 
+
+
 # Get the transformed images:
 img = transformed_images[0]
 img_st = transformed_images[1]
 label = transformed_images[2]
+
+img = transforms_inten(img)
 
 img = torch.moveaxis(img,0,-1)
 img_st = torch.moveaxis(img_st,0,-1)
