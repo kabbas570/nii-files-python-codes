e423b588e148d00c414d7d1a7f31f64d7a88e355
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2025-06-06 16:33:00 +0100

    Update labcut.py

--- labcut.py
+++ labcut.py
@@ -1,3 +1,9 @@
+from deeplabcut.modelzoo.video_inference import video_inference_superanimal
+videos = ['/content/drive/MyDrive/Latest_DeepLabCut/videos/vid1/An1_2_30minutes_IL_31_5.mp4']
+out_dir = '/content/drive/MyDrive/Latest_DeepLabCut/videos/out_dir1/'
+superanimal_name = "superanimal_topviewmouse"
+videotype = "mp4"
+scale_list = [300]
 video_inference_superanimal(
     videos,
     superanimal_name,
